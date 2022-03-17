696f183b27d374fc8efbed4885f7c9a8d8d8f6e4
Nawel Benaicha <nawelbenaicha@MacBook-Pro-de-Nawel.local>
2022-03-17 15:09:20 +0100

    STYLE: MAJ du scss + tentative de JS pour le submit button

--- form.js
+++ form.js
@@ -1,1 +1,8 @@
- 
+const message =
+"Merci de nous avoir contacté, nous faisons le necessaire afin de répondre au mieux à votre demande";
+
+document.getElementsByClassName("contact__form")
+.addEventListener("submit", function (event) {
+  event.preventDefault();
+  alert(message);
+});
