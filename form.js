31a65981e51b6fd0ab050ff93206df657ce2e546
Nawel Benaicha <nawelbenaicha@MacBook-Pro-de-Nawel.local>
2022-03-19 12:55:23 +0100

    CODE: ajustement hover

--- form.js
+++ form.js
@@ -1,4 +1,4 @@
+// POP UP -> SUBMIT
 document.getElementById("submit").addEventListener("click", function() {
   alert("Merci d'avoir choisi notre établissement. Nous faisons le nécessaire pour répondre au mieux à votre demande. A très vite, L'équipe du Cenitz restaurant.");
 });
-
